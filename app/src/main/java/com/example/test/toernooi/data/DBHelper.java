4b83a721ef46d2bd219dca2a2da10bfe6669cfcc
Melanie Backers <melanie.backers@hva.nl>
2017-11-05 15:46:10 +0100

    project is werkend

--- app/src/main/java/com/example/test/toernooi/data/DBHelper.java
+++ app/src/main/java/com/example/test/toernooi/data/DBHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "badmintontoernooi.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 4;
 
     // Creating the table
     private static final String DATABASE_CREATE =
@@ -19,17 +19,19 @@ public class DBHelper extends SQLiteOpenHelper {
                     "(" +
                     ToernooiContract.ToernooiEntry.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT ,"
                     + ToernooiContract.ToernooiEntry.COLUMN_NAME_NAAM + " TEXT, "
-                    + ToernooiContract.ToernooiEntry.COLUMN_NAME_DATUM + " TEXT )" +
+                    + ToernooiContract.ToernooiEntry.COLUMN_NAME_DATUM + " TEXT )"
+//                    +
 
-            "CREATE TABLE " + SpelerContract.SpelerEntry.TABLE_NAME +
-                    "(" +
-                    SpelerContract.SpelerEntry.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT ,"
-                    + SpelerContract.SpelerEntry.COLUMN_NAME_NAAM + " TEXT, "
-                    + SpelerContract.SpelerEntry.COLUMN_NAME_GEBOORTEDATUM + " TEXT, "
-                    + SpelerContract.SpelerEntry.COLUMN_NAME_CLUB + " TEXT, "
-                    + SpelerContract.SpelerEntry.COLUMN_NAME_SOORT_LID + " TEXT, "
-                    + SpelerContract.SpelerEntry.COLUMN_NAME_SPEELSTERKTE + " TEXT, "
-                    + SpelerContract.SpelerEntry.COLUMN_NAME_COMPETITIE + " TEXT )";
+//            "CREATE TABLE " + SpelerContract.SpelerEntry.TABLE_NAME +
+//                    "(" +
+//                    SpelerContract.SpelerEntry.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT ,"
+//                    + SpelerContract.SpelerEntry.COLUMN_NAME_NAAM + " TEXT, "
+//                    + SpelerContract.SpelerEntry.COLUMN_NAME_GEBOORTEDATUM + " TEXT, "
+//                    + SpelerContract.SpelerEntry.COLUMN_NAME_CLUB + " TEXT, "
+//                    + SpelerContract.SpelerEntry.COLUMN_NAME_SOORT_LID + " TEXT, "
+//                    + SpelerContract.SpelerEntry.COLUMN_NAME_SPEELSTERKTE + " TEXT, "
+//                    + SpelerContract.SpelerEntry.COLUMN_NAME_COMPETITIE + " TEXT )"
+            ;
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -43,7 +45,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + ToernooiContract.ToernooiEntry.TABLE_NAME);
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + SpelerContract.SpelerEntry.TABLE_NAME);
+//        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + SpelerContract.SpelerEntry.TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
 }
